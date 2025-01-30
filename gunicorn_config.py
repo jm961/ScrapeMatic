cfbb6fc0445e6438f64e0c632e30e8fec294cf7e
jm_eid <jeanmicheleid@gmail.com>
2025-01-30 23:10:26 +0200

    fixed the config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,21 @@
-bind = "0.0.0.0:80"
+import multiprocessing
 
-workers = 4
+# Bind to a Unix socket instead of a network port
+bind = "unix:/var/www/ScrapeMatic.sock"
+
+# Number of worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# Worker class
+worker_class = "sync"
+
+# Timeout in seconds
+timeout = 120
+
+# Logging
+accesslog = "/var/www/ScrapeMatic/logs/gunicorn_access.log"
+errorlog = "/var/www/ScrapeMatic/logs/gunicorn_error.log"
+loglevel = "info"
+
+# Daemonization
+daemon = False
