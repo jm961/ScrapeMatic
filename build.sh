81a327d73ad5610aeddd91ca04e9c641d166037a
jm_eid <jeanmicheleid@gmail.com>
2025-02-01 11:32:50 +0200

    added deps

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 # Upgrade pip
@@ -7,5 +6,12 @@ pip install --upgrade pip
 # Install required Python packages
 pip install -r requirements.txt
 
+# Install system libraries
+apt-get update && apt-get install -y \
+    libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 \
+    libdrm2 libxcomposite1 libxdamage1 libxfixes3 \
+    libxrandr2 libgbm1 libxkbcommon0 libasound2 \
+    libatspi2.0-0
+
 # Install Playwright browsers
-playwright install chromiumg
+playwright install chromium
